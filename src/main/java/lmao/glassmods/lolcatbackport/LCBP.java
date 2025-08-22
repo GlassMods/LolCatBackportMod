82460d8deaa3ac4ab734b810c099168b11afc48d
Bread Cat <105464811+gatooooooo@users.noreply.github.com>
2025-08-22 15:39:41 +0100

    why? idfk

---
+++ src/main/java/lmao/glassmods/lolcatbackport/LCBP.java
@@ -0,0 +1,8 @@
+package lmao.glassmods.lolcatbackport;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "lcbp", name = "Lol Cat Backport", version = "1.0", acceptedMinecraftVersions = "[1.8.9]", clientSideOnly = true)
+public class LCBP {
+
+}
---
+++ src/main/java/lmao/glassmods/lolcatbackport/mixins/MixinLanguageManager.java
@@ -0,0 +1,27 @@
+package lmao.glassmods.lolcatbackport.mixins;
+
+import com.google.common.collect.Maps;
+import net.minecraft.client.resources.IResourcePack;
+import net.minecraft.client.resources.Language;
+import net.minecraft.client.resources.LanguageManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+
+@Mixin(LanguageManager.class)
+public abstract class MixinLanguageManager {
+
+    @Shadow private Map<String, Language> languageMap = Maps.newHashMap();
+
+    @Inject(method = "parseLanguageMetadata", at = @At("RETURN"))
+    private void injectLolCatLanguage(List<IResourcePack> resourcesPacks, CallbackInfo ci) {
+        Language lolCat = new Language("lol_koc", "Kingdum ov cats", "Lolcat", false);
+        languageMap.put("lol_koc", lolCat);
+    }
+    
+}
